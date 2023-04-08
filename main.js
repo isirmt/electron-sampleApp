97c588226559cd15286d6df20fa8ef6a1ffd0979
isirmt <r119306@g.kobe-kosen.ac.jp>
2023-04-09 02:09:08 +0900

    update title bar

--- main.js
+++ main.js
@@ -7,9 +7,15 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#2f3241',
+      symbolColor: '#74b1be',
+      height: 10
+    },
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      webPreferences: { nodeIntegration : false }
+      webPreferences: { nodeIntegration : false, devTools: false }
     },
   });
 
