977296d6b2de445d349faeb552823ee7077089b7
isirmt <r119306@g.kobe-kosen.ac.jp>
2023-04-09 00:43:56 +0900

    initial

---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, ipcMain, shell } = require('electron');
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      webPreferences: { nodeIntegration : false }
+    },
+  });
+
+  const handleUrlOpen = (e, url)=>{
+    if( url.match(/^http/)){
+      e.preventDefault()
+      shell.openExternal(url)
+    }
+  }
+  win.webContents.on('will-navigate', handleUrlOpen);
+  win.webContents.on('new-window', handleUrlOpen);
+
+  ipcMain.handle('ping', () => 'pong')
+  win.setMenuBarVisibility(false);
+  win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
